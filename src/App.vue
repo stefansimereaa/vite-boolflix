b85b13f159b990175607b069ff9bc8513ad1dfc2
stefansimereaa <stefanosimerea16@gmail.com>
2023-07-05 13:01:00 +0200

    added component appheader

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <script>
+import axios from 'axios';
+import { store } from './data/store.js';
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
+const endpoint = ''
+
+
+export default {
+  data() {
+    return {
+      components: { AppHeader, AppMain, },
+    }
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <div class="container-background">
+    <!-- Component App Header -->
+    <AppHeader />
+    <!-- Component App Main -->
+    <AppMain />
+  </div>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+@import '../src/assets/scss/style.scss';
+</style>
+
